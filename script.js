4b09358977811d65259c47d53d4cb85262d76bd0
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-05-13 16:00:59 -0500

    feat: :sparkles: Permite poner los discos en las torres
    Se ha hecho la funcion para que se coloque el disco que se arrastre en la torre que se selecione

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 const discos=document.querySelectorAll(".disco")
 const torres=document.querySelectorAll(".torre")
 
-/***CONCEPTOS IMPORTANTES 
- * 
- * 1. dragstart=cuando comienza arrastrarse
- * 2.dragemd=cuando soltamos el mouse
- * 
- *3.dragenter=cuando el contenedor detecta algo
-  4.dragleave=cuando ya no detecta nada
-  5. dragover=disco en una sona valida, no neceseariamente bien adentro
-  6. drop=se solto en una zona valida
-  */
 
-
-torres.forEach(torre=>{
-    torre.addEventListener("dragenter", (e)=>{
-        console.log("Disco adentro");
+discos.forEach((disco)=>{
+    disco.addEventListener("dragstart", (e)=>{
+        console.log(e.target.id);
+        e.dataTransfer.setData("text/plain", e.target.id)
     })
-    torre.addEventListener("dragleave", (e)=>{
-        console.log("disco Afuera");
-    })
-    torre.addEventListener("dragover", (e)=>{
-        e.preventDefault() //permite soltar y no salirse
-        console.log("disco en una sona valida");
-    })
-    torre.addEventListener("drop", (e)=>{
-        console.log("se solto en una sona valida");
-        torre.appendChild(discos[5])
+    disco.addEventListener("dragend", (e)=>{
+        e.dataTransfer.clearData();
     })
 })
+
+torres.forEach((torre)=>{
+    torre.addEventListener("dragover", (e)=>{
+        e.preventDefault() //permite soltar y no salirse
+   
+    })
+    torre.addEventListener("drop", soltarDisco)
+})
+
+
+function soltarDisco(e) {
+    e.preventDefault();
+    const discoId = e.dataTransfer.getData('text/plain');
+    const disco=document.getElementById(discoId)
+    e.target.appendChild(disco)
+}
+
+
+
