6aa7de5ac4dafcc9a9ca38ade0aa85ce2fb83054
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-05-13 19:44:09 -0500

    refactor: :recycle: Formato al documento para mejor lectura

--- js/logicaRanking.js
+++ js/logicaRanking.js
@@ -1,55 +1,46 @@
-import{getuser, addUser} from "./metodosApi.js"
+import { getuser, addUser } from "./metodosApi.js";
 import logica from "./logicaJuego.js";
 
-
-
- const obtenerUser=async()=>{
-    const response=await getuser();
-    const ranking=response.sort((a, b) => {
-        const tiempoA = convertirTiempo(a.tiempo);
-        const tiempoB = convertirTiempo(b.tiempo);
-        return tiempoA - tiempoB;
-      });
-    const container=document.querySelector(".tableRanking")
-    let html="";
-    ranking.map((info)=>{
-        html+=`
+const obtenerUser = async () => {
+  const response = await getuser();
+  const ranking = response.sort((a, b) => {
+    const tiempoA = convertirTiempo(a.tiempo);
+    const tiempoB = convertirTiempo(b.tiempo);
+    return tiempoA - tiempoB;
+  });
+  const container = document.querySelector(".tableRanking");
+  let html = "";
+  ranking.map((info) => {
+    html += `
             <tr>
                 <td>${info.id}</td>
                 <td>${info.name}</td>
                 <td>${info.tiempo}</td>
             </tr>
-        `
-    })
-    container.insertAdjacentHTML("beforeend", html)
-    
+        `;
+  });
+  container.insertAdjacentHTML("beforeend", html);
+};
+
+const guardarRegistro = () => {
+  const input = document.getElementById("nombre").value;
+  const name = input;
+  const tiempo = logica.finalizarJuego();
+
+  const datos = {
+    name,
+    tiempo,
+  };
+
+  addUser(datos);
+};
+
+function convertirTiempo(tiempo) {
+  const partes = tiempo.split(":");
+  const horas = parseInt(partes[0], 10);
+  const minutos = parseInt(partes[1], 10);
+  const segundos = parseInt(partes[2], 10);
+  return horas * 3600 + minutos * 60 + segundos;
 }
 
-const guardarRegistro=()=>{
-    
-        const input=document.getElementById("nombre").value
-        const name=input
-        const tiempo=logica.finalizarJuego();
-    
-        
-        const datos={
-            name,
-            tiempo
-        }
-    
-        addUser(datos)
-      
-}
-
-
-
-  function convertirTiempo(tiempo) {
-    const partes = tiempo.split(":");
-    const horas = parseInt(partes[0], 10);
-    const minutos = parseInt(partes[1], 10);
-    const segundos = parseInt(partes[2], 10);
-    return horas * 3600 + minutos * 60 + segundos;
-  }
-
-
-export default { obtenerUser, guardarRegistro }
+export default { obtenerUser, guardarRegistro };
